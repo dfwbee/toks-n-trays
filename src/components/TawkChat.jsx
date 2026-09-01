2c85116b90a2513036e1150e309a6d2fa37a1c04
Melody <bibakingbehin@gmail.com>
2026-09-01 07:48:26 +0100

    Added Tawk Chat IDs

--- src/components/TawkChat.jsx
+++ src/components/TawkChat.jsx
@@ -1,8 +1,8 @@
 import { useEffect } from "react";
 
 
-const TAWK_PROPERTY_ID = "YOUR_PROPERTY_ID";
-const TAWK_WIDGET_ID = "YOUR_WIDGET_ID";
+const TAWK_PROPERTY_ID = "6a9674428a6e1f344847f2ab";
+const TAWK_WIDGET_ID = "1k1drc9m0";
 
 export default function TawkChat() {
   useEffect(() => {
